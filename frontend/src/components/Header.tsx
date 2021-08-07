370c08ca2da5bf4ae16da994e225a05173ec70a5
Felipe <felipecrs32@gmail.com>
2021-08-07 13:06:04 -0300

    Header component and update in the code

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,18 @@
-import Image from "next/image";
-
 import styles from "../styles/components/Header.module.scss";
 
 export default function Header() {
   return (
-      <header className={styles.header}>
-          <img src="/logo.svg" alt="HELP.FOOD" />
-          <nav>
-              <a href="#">Início</a>
-              <a href="#">Receitas</a>
-              <a href="#">Favoritos</a>
-          </nav>
-      </header>
+    <header className={styles.header}>
+      <img src="/logo.svg" alt="HELP.FOOD" />
+      <nav>
+        <a href="#">Início</a>
+        <a href="#">Receitas</a>
+        <a href="#">Favoritos</a>
+      </nav>
+      <div className={styles.buttons}>
+        <button className={styles.buttonNotFilled}>Login</button>
+        <button className={styles.buttonFilled}>Cadastrar</button>
+      </div>
+    </header>
   );
 }
